fc86ad33279b5b14212c97b8891e6a4997bbf8e5
Andrey <70208784+andreystashev@users.noreply.github.com>
2023-10-04 00:11:04 +0300

    8 task done

--- homework_04.py
+++ homework_04.py
@@ -160,6 +160,9 @@ def task_07():
 # task_07()
 
 
+# Упражнение 8.
+
+
 class Storm:
     def info(self):
         print("Storm")
@@ -203,6 +206,7 @@ class Earth:
             return Dust()
 
 
+
 class Fire:
     def info(self):
         print("Fire")
@@ -214,6 +218,7 @@ class Fire:
             return Steam()
         elif type(other) is Air:
             return Lightning()
+
 
 
 class Water:
@@ -229,6 +234,7 @@ class Water:
             return Storm()
 
 
+
 class Air:
     def info(self):
         print("Air")
@@ -242,9 +248,96 @@ class Air:
             return Storm()
 
 
-e = Earth()
-f = Fire()
-w = Water()
-a = Air()
-s = w + a
-s.info()
+class FifthElement:
+    def info(self):
+        print("Leeloo Dallas! Multi-pass!")
+
+    def __add__(self, other):
+        return self
+
+def task_08():
+    e = Earth()
+    f = Fire()
+    w = Water()
+    a = Air()
+    fe = FifthElement()
+
+    s = f + a
+    if s != None:
+        s.info()
+
+# task_08()
+
+
+class NoMoneyToWithdrawError(Exception):
+   def __init__(self, message):
+       super().__init__(message)
+
+
+class PaymentError(Exception):
+   def __init__(self, message):
+       super().__init__(message)
+
+
+def print_accounts(accounts):
+   """Печать аккаунтов."""
+   print("Список клиентов ({}): ".format(len(accounts)))
+   for i, (name, value) in enumerate(accounts.items(), start=1):
+       print("{}. {} {}".format(i, name, value))
+
+
+def transfer_money(accounts, account_from, account_to, value):
+   """Выполнить перевод 'value' денег со счета 'account_from' на 'account_to'.
+
+   При переводе денежных средств необходимо учитывать:
+       - хватает ли денег на счету, с которого осуществляется перевод;
+       - перевод состоит из уменьшения баланса первого счета и увеличения
+         баланса второго; если хотя бы на одном этапе происходит ошибка,
+         аккаунты должны быть приведены в первоначальное состояние
+         (механизм транзакции)
+         см. https://ru.wikipedia.org/wiki/Транзакция_(информатика).
+
+   Исключения (raise):
+       - NoMoneyToWithdrawError: на счету 'account_from'
+                                 не хватает денег для перевода;
+       - PaymentError: ошибка при переводе.
+   """
+   # Удалите комментарий и допишите код
+
+
+if __name__ == "__main__":
+   accounts = {
+       "Василий Иванов": 100,
+       "Екатерина Белых": 1500,
+       "Михаил Лермонтов": 400
+   }
+   print_accounts(accounts)
+
+   payment_info = {
+       "account_from": "Екатерина Белых",
+       "account_to": "Василий Иванов"
+   }
+
+   print("Перевод от {account_from} для {account_to}...".
+         format(**payment_info))
+
+   payment_info["value"] = int(input("Сумма = "))
+
+   transfer_money(accounts, **payment_info)
+
+   print("OK!")
+
+   print_accounts(accounts)
+
+def task_10():
+    summ = 0.
+    while True:
+        num = input("Введите число.")
+        if num.isdigit():
+            summ += float(num)
+            print(f"сумма: {summ}")
+        elif num == "":
+            print("Выходим.")
+            break
+        else:
+            print("Это не число.")
